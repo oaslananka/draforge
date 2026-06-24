1b8243b1ec6b29be1e2db9359bb374984068e987
oaslananka <285490571+oaslananka@users.noreply.github.com>
2026-06-24 21:20:36 +0300

    p32-controller-runtime

---
+++ cmd/draforge-controller/runtime.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
+	"time"
+
+	"github.com/oaslananka/draforge/internal/simulator"
+)
+
+func newRuntimeServer(addr string, reconciler *simulator.Reconciler) *http.Server {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/healthz", okHandler)
+	mux.HandleFunc("/readyz", okHandler)
+	mux.HandleFunc("/metrics", metricsHandler(reconciler))
+	return &http.Server{
+		Addr:              addr,
+		Handler:           mux,
+		ReadHeaderTimeout: 5 * time.Second,
+		ReadTimeout:       10 * time.Second,
+		WriteTimeout:      10 * time.Second,
+		IdleTimeout:       60 * time.Second,
+	}
+}
+
+func okHandler(w http.ResponseWriter, _ *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write([]byte("ok\n"))
+}
+
+func metricsHandler(reconciler *simulator.Reconciler) http.HandlerFunc {
+	return func(w http.ResponseWriter, _ *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; version=0.0.4; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		_, _ = fmt.Fprintf(w, "# HELP draforge_controller_reconcile_errors_total Total number of reconciliation errors\n")
+		_, _ = fmt.Fprintf(w, "# TYPE draforge_controller_reconcile_errors_total counter\n")
+		_, _ = fmt.Fprintf(w, "draforge_controller_reconcile_errors_total %d\n", atomic.LoadInt64(&reconciler.ReconcileErrorsCount))
+		_, _ = fmt.Fprintf(w, "draforge_controller_allocations_simulated_total %d\n", atomic.LoadInt64(&reconciler.AllocationsSimulated))
+		_, _ = fmt.Fprintf(w, "draforge_controller_active_faults %d\n", atomic.LoadInt64(&reconciler.ActiveFaultsCount))
+	}
+}
